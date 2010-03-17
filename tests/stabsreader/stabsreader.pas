34943f1a3eeb0098db66dcf3a223987b257d8e1b
dmitry boyarintsev <skalogryz.lists@gmail.com>
2010-03-17 08:20:33 +0000

    extended stabs test with typing scanned information
    git-svn-id: https://duby.svn.sourceforge.net/svnroot/duby@149 fe2c8e78-f329-46f0-b40f-d814d8d8a4bb

--- tests/stabsreader/stabsreader.pas
+++ tests/stabsreader/stabsreader.pas
@@ -3,9 +3,6 @@ program stabsreader;
 {$mode objfpc}{$H+}
 
 uses
-  {$IFDEF UNIX}{$IFDEF UseCThreads}
-  cthreads,
-  {$ENDIF}{$ENDIF}
   Classes, dbgInfoTypes, dbgInfoStabs, stabs,
   elfDbgSource,
   machoDbgSource, PESource, stabsproc;
@@ -25,11 +22,6 @@ end;
 procedure ReadStabsData(source : TDbgDataSource);
 var
   stabs : TDbgStabsInfo;
-
-  addr  : Integer;
-  nm    : WideString;
-  i     : LongWord;
-
   names : TStringList;
 begin
   if not TDbgStabsInfo.isPresent(source) then begin
@@ -41,67 +33,16 @@ begin
   stabs := TDbgStabsInfo.Create(source);
   writeln('stabs read.');  
 
-  {writeln('type addr. 0 - to exit');
-  ReadLn(addr);
-  while addr <> 0 do begin
-    if stabs.GetLineByAddr(addr, nm, i) then begin
-      writeln(GetFileString(nm, i));
-    end else 
-      writeln('not found');
-    
-    ReadLn(addr);
-  end;}
-
-{  names := TStringList.Create;
-  stabs.GetNames(names, '');
-  for i := 0 to names.Count - 1 do  writeln(names[i]);
-  names.Free;
-}
-
   stabs.dump_symbols;
-
   stabs.Free;
-end;
-
-procedure StabsParsing;
-var
-  ststr   : string;
-  name    : string;
-  mdstr   : string;
-  mdnum   : Integer;
-  value   : string;
-  typeval : string;
-
-  structSize : Integer;
-  elemIndex  : Integer;
-
-  bitofs     : Integer;
-  bitsize    : Integer;
-begin
-  ststr := 'SHORTSTRING:Tt6=s256length:1,0,8;st:ar1;1;255;8,8,2040;;';
-  ParseStabStr(ststr, name, mdstr, mdnum, value);
-  writeln('str      = "', ststr,'"');
-  writeln('name     = "', name,'"');
-  writeln('modifier = "', mdstr,'"');
-  writeln('mod num  = "', mdnum,'"');
-  writeln('value    = "', value,'"');
-
-  if isStructType(value, StructSize, elemIndex) then begin
-    writeln('struct size = ', structSize);
-    writeln('first index = ', elemIndex);
-    writeln('length(value) = ', length(value));
-    while GetStructElem(value, elemIndex, name, typeval, bitofs, bitsize, elemIndex) do
-      writeln('  ', name, ' type = "', typeval, '"; ofs = ', bitofs, ' size = ', bitsize);
-  end else
-    writeln('not struct!');
-  Halt(0);
 end;
 
 var
   dbgInfoSrc : TDbgDataSource;
-begin
-  //StabsParsing;
 
+{$R *.res}
+
+begin
   if Paramcount < 1 then begin
     writeln('please specify stabs debug-info file name');
     Exit;

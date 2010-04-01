9b520c2f0ce5330dd36acfde6ee38e1a54ca7bc2
dmitry boyarintsev <skalogryz.lists@gmail.com>
2010-04-01 14:19:19 +0000

    added dbginfopascal.pas
    git-svn-id: https://duby.svn.sourceforge.net/svnroot/duby@170 fe2c8e78-f329-46f0-b40f-d814d8d8a4bb

---
+++ dubylib/dbginfopascal.pas
@@ -0,0 +1,26 @@
+unit dbgInfoPascal;
+
+interface
+
+uses
+  dbgInfoTypes;
+
+// RTL pascal functions... if any available?!
+procedure InitRTLSymbols(info: TDbgInfo);
+
+// Strings, Files, dynamic arrays, objects and classes
+procedure InitStdTypes(info: TDbgInfo);
+
+implementation
+
+procedure InitRTLSymbols(info: TDbgInfo);
+begin
+  //todo:
+end;
+
+procedure InitStdTypes(info: TDbgInfo);
+begin
+  //todo:
+end;
+
+end.

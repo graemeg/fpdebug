80b95899edbc4511ed1298dde63c8304615a4437
Graeme Geldenhuys <graemeg@gmail.com>
2016-04-23 01:04:43 +0100

    elf unit: trims trailing whitespace. No other changes were made.

--- debuglib/elf.pas
+++ debuglib/elf.pas
@@ -21,7 +21,7 @@ type
   Elf32_Off   = UInt32; // Usigned file offset
   Elf32_Sword = Int32;  // Signed large integer
   Elf32_Word  = UInt32; // Usigned large integer
-  
+
   Elf64_Addr   = UInt64; // Unsigned program address
   Elf64_Off    = UInt64; // Unsigned ﬁle offset
   Elf64_Half   = UInt16; // Unsigned medium integer
@@ -31,22 +31,22 @@ type
   Elf64_Sxword = Int64;  // Signed long integer
 
 const
-  EINDENT = 16;  
+  EINDENT = 16;
 
-type 
+type
   elf_ident = packed record
   case byte of
    0: (e_ident: array [0..EINDENT-1] of byte);
-   1: (id_mag        : array[0..3] of Char;          
-       id_class      : Byte;               
-       id_data       : Byte;               
-       id_version    : Byte;              
-       id_OSABI      : Byte;              
-       id_AbiVersion : Byte;              
+   1: (id_mag        : array[0..3] of Char;
+       id_class      : Byte;
+       id_data       : Byte;
+       id_version    : Byte;
+       id_OSABI      : Byte;
+       id_AbiVersion : Byte;
       );
   end;
 
-const  
+const
   EI_MAG0       = 0; // $7F
   EI_MAG1       = 1; // E
   EI_MAG2       = 2; // L
@@ -56,7 +56,7 @@ const
   EI_VERSION    = 1;
   EI_OSABI      = 1;
   EI_ABIVERSION = 1;
-  
+
   ELFMAGIC     = chr($7f)+'ELF';
 
   //elf class
@@ -68,14 +68,14 @@ const
   ELFDATANONE  = 0;
   ELFDATA2LSB  = 1;
   ELFDATA2MSB  = 2;
-  
-  // Operating System and ABI Identifiers, e_ident[EI_OSABI] 
-  ELFOSABI_SYSV = 0;  // System V ABI 
-  ELFOSABI_HPUX = 1;  // HP-UX operating system 
-  ELFOSABI_STANDALONE = 255; // Standalone (embedded) application 
-  
-  
-type  
+
+  // Operating System and ABI Identifiers, e_ident[EI_OSABI]
+  ELFOSABI_SYSV = 0;  // System V ABI
+  ELFOSABI_HPUX = 1;  // HP-UX operating system
+  ELFOSABI_STANDALONE = 255; // Standalone (embedded) application
+
+
+type
   //note: it doesn't include Ident block
   Elf32_EHdr = packed record
     e_ident       : elf_ident;    { ELF identification }
@@ -94,7 +94,7 @@ type
     e_shstrndx    : Elf32_Half;   { Section name string table index }
   end;
   PElf32_EHdr = ^Elf32_EHdr;
-  
+
   Elf64_EHdr = packed record
     e_ident       : elf_ident;    { ELF identification }
     e_type        : Elf64_Half;   { Object file type }
@@ -112,19 +112,19 @@ type
     e_shstrndx    : Elf64_Half;   { Section name string table index }
   end;
   PElf64_EHdr = ^Elf64_EHdr;
-  
+
 const
   // object file type {Elf32_Hdr.e_type}
   ET_NONE      = 0;     // No ﬁle type
   ET_REL       = 1;     // Relocatable object ﬁle .o
-  ET_EXEC      = 2;     // Executable ﬁle 
+  ET_EXEC      = 2;     // Executable ﬁle
   ET_DYN       = 3;     // Shared object ﬁle .so
   ET_CORE      = 4;     // Core ﬁle
   ET_LOOS      = $fe00; // os-specific
   ET_HIOS      = $feff;
   ET_LOPROC    = $ff00; // processor-specific
   ET_HIPROC    = $ffff;
-  
+
   // machine type     {Elf32_Hdr.e_machine}
   EM_NONE        = 0;
   EM_SPARC       = 2;
@@ -146,11 +146,11 @@ const
   SHN_LORESERVE = $ff00; {This value speciﬁes the lower bound of the range of reserved indexes.}
   SHN_LOPROC    = $ff00;
   SHN_HIPROC    = $ff1f;
-  SHN_ABS       = $fff1; // Indicates that the corresponding reference is an absolute value 
+  SHN_ABS       = $fff1; // Indicates that the corresponding reference is an absolute value
   SHN_COMMON    = $fff2; // Indicates a symbol that has been declared as a common block (Fortran COMMON or C tentative declaration)
   SHN_HIRESERVE = $ffff;
 
-type  
+type
   Elf32_shdr = packed record
     sh_name      : Elf32_Word;
     sh_type      : Elf32_Word;
@@ -164,7 +164,7 @@ type
     sh_entsize   : Elf32_Word;
   end;
   PElf32_shdr = ^Elf32_shdr;
-  
+
   Elf64_Shdr = packed record
     sh_name      : Elf64_Word;   // Section name
     sh_type      : Elf64_Word;   // Section type
@@ -178,11 +178,11 @@ type
     sh_entsize   : Elf64_Xword;  // Size of entries, if section has table
   end;
   PElf64_Shdr = ^Elf64_Shdr;
-  
+
 
 const
   //section type
-  SHT_NULL     = 0;         // Marks an unused section header 
+  SHT_NULL     = 0;         // Marks an unused section header
   SHT_PROGBITS = 1;         // Contains information deﬁned by the program
   SHT_SYMTAB   = 2;         // Contains a linker symbol table
   SHT_STRTAB   = 3;         // Contains a string table
@@ -191,14 +191,14 @@ const
   SHT_DYNAMIC  = 6;         // Contains dynamic linking tables
   SHT_NOTE     = 7;         // Contains note information
   SHT_NOBITS   = 8;         // Contains uninitialized space; does not occupy any space in the ﬁle
-  SHT_REL      = 9;         // Contains “Rel” type relocation entries 
+  SHT_REL      = 9;         // Contains “Rel” type relocation entries
   SHT_SHLIB    = 10;        // Reserved
   SHT_DYNSYM   = 11;        // Contains a dynamic loader symbol table
-  
+
   SHT_LOOS     = $60000000; // Environment-speciﬁc use
   SHT_HIOS     = $6fffffff;
   SHT_LOPROC   = $70000000; // Processor-speciﬁc use
-  SHT_HIPROC   = $7fffffff; 
+  SHT_HIPROC   = $7fffffff;
 
   //section attribute flags
   SHF_WRITE     = 1;          // Section contains writable data
@@ -206,7 +206,7 @@ const
   SHF_EXECINSTR = 4;          // Section contains executable instructions
   SHF_MASKOS    = $0f000000;  // Environment-speciﬁc use
   SHF_MASKPROC  = $f0000000;  // Processor-speciﬁc use
-  
+
 
 type
   Elf32_Sym = packed record
@@ -218,9 +218,9 @@ type
     st_shndx   : Elf32_Half;
   end;
   PElf32_Sym = ^Elf32_Sym;
-  
+
   Elf64_Sym = packed record
-    st_name    : Elf64_Word;   // Symbol name 
+    st_name    : Elf64_Word;   // Symbol name
     st_info    : Byte;         // Type and Binding attributes
     st_other   : Byte;         // Reserved
     st_shndx   : Elf64_Half;   // Section table index
@@ -228,8 +228,8 @@ type
     st_Size    : Elf64_Xword   // Size of object (e.g., common)
   end;
   PElf64_Sym = ^Elf64_Sym;
-  
-  
+
+
 const
   //symbol bindings
   STB_LOCAL  = 0;    // Not visible outside the object ﬁle
@@ -239,10 +239,10 @@ const
   STB_HIOS   = 12;
   STB_LOPROC = 13;   // Processor-speciﬁc use
   STB_HIPROC = 15;
-  
+
   //symbol types
   STT_NOTYPE         = 0;  // No type speciﬁed (e.g., an absolute symbol)
-  STT_OBJECT         = 1;  // Data object 
+  STT_OBJECT         = 1;  // Data object
   STT_FUNC           = 2;  // Function entry point
   STT_SECTION        = 3;  // Symbol is associated with a section
   STT_FILE           = 4;  // Source ﬁle associated with the object ﬁle
@@ -251,20 +251,20 @@ const
   STT_LOOS           = 10; // Environment-speciﬁc use
   STT_HIOS           = 12;
   STT_LOPROC         = 13; // Processor-speciﬁc use
-  STT_SPARC_REGISTER = 13;  
+  STT_SPARC_REGISTER = 13;
   STT_HIPROC         = 15;
 
 
-  
+
 {
   Relocation
 
-  Relocation is the process of connecting symbolic references with symbolic 
-  deﬁnitions. For example, when a program calls a function, the associated call 
-  instruction must transfer control to the proper destination address at execution. 
-  In other words, relocatable ﬁles must have information that describes how 
-  to modify their section contents, thus allowing executable and shared object 
-  ﬁles to hold the right information for a process’s program image. 
+  Relocation is the process of connecting symbolic references with symbolic
+  deﬁnitions. For example, when a program calls a function, the associated call
+  instruction must transfer control to the proper destination address at execution.
+  In other words, relocatable ﬁles must have information that describes how
+  to modify their section contents, thus allowing executable and shared object
+  ﬁles to hold the right information for a process’s program image.
   Relocation entriesare these data.
 }
 
@@ -273,28 +273,28 @@ type
     r_offset : Elf32_Addr;
     r_info   : Elf32_Word;
   end;
-  
+
   Elf32_rela = packed record
     r_offset : Elf32_Addr;
     r_info   : Elf32_Word;
     r_addend : Elf32_Sword;
   end;
 
-{ r_offset This member gives the location at which to apply the relocation action. For a relocatable 
-           file, the value is the byte offset from the beginning of the section to the storage unit affected 
-           by the relocation. For an executable ﬁle or a shared object, the value is the virtual address of 
-           the storage unit affected by the relocation. 
-  r_info   This member gives both the symbol table index with respect to which the relocation must be 
-           made, and the type of relocation to apply. For example, a call instruction’s relocation entry 
-           would hold the symbol table index of the function being called. If the index isSTN_UNDEF, 
-           the undeﬁned symbol index, the relocation uses 0 as the ‘‘symbol value.’’ Relocation types 
-           are processor-speciﬁc. When the text refers to a relocation entry’s relocation type or symbol 
-           table index, it means the result of applyingELF32_R_TYPEorELF32_R_SYM, respectively, 
-           to the entry’sr_infomember   }      
+{ r_offset This member gives the location at which to apply the relocation action. For a relocatable
+           file, the value is the byte offset from the beginning of the section to the storage unit affected
+           by the relocation. For an executable ﬁle or a shared object, the value is the virtual address of
+           the storage unit affected by the relocation.
+  r_info   This member gives both the symbol table index with respect to which the relocation must be
+           made, and the type of relocation to apply. For example, a call instruction’s relocation entry
+           would hold the symbol table index of the function being called. If the index isSTN_UNDEF,
+           the undeﬁned symbol index, the relocation uses 0 as the ‘‘symbol value.’’ Relocation types
+           are processor-speciﬁc. When the text refers to a relocation entry’s relocation type or symbol
+           table index, it means the result of applyingELF32_R_TYPEorELF32_R_SYM, respectively,
+           to the entry’sr_infomember   }
 
 
   Elf64_Rel = packed record
-    r_offset : Elf64_Addr;    // Address of reference 
+    r_offset : Elf64_Addr;    // Address of reference
     r_info   : Elf64_Xword;   // Symbol index and type of relocation
   end;
   PElf64_Rel = ^Elf64_Rel;
@@ -305,8 +305,8 @@ type
     r_addend : Elf64_Sxword;  // Constant part of expression
   end;
   PElf64_Rela = ^Elf64_Rela;
-           
-           
+
+
 const
   R_386_NONE     = 0;
   R_386_32       = 1;
@@ -320,15 +320,15 @@ const
   R_386_GOTOFF   = 9;
   R_386_GOTPC    = 10;
 
- 
+
 function Elf32_R_Sym(i: LongWord): LongWord; inline;
 function Elf32_R_Type(i: LongWord): LongWord; inline;
 function Elf32_R_Info(s, t: LongWord): LongWord; inline;
 
 function Elf64_R_Sym(i: QWord): QWord; inline;
-function Elf64_R_Type(i: QWord): QWord; inline;  
-function Elf64_R_Info(s, t: QWord): QWord; inline;  
-  
+function Elf64_R_Type(i: QWord): QWord; inline;
+function Elf64_R_Info(s, t: QWord): QWord; inline;
+
 implementation
 
 function Elf32_R_Sym(i: LongWord): LongWord; inline;
@@ -346,7 +346,7 @@ begin
   Result := (s shl 8) + (t and $FF);
 end;
 
-function Elf64_R_Info(s, t: QWord): QWord; inline; 
+function Elf64_R_Info(s, t: QWord): QWord; inline;
 begin
   Result := (s shl 32) + (t and $FFFFFFFF);
 end;
